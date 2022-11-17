38b2257307c0919616b41339d8dec4dfa1ffdb2e
GwenMurphy <gwenmurphy1997@googlemail.com>
2022-11-17 14:44:24 +0000

    Comment errs sorted. Bossman's very much a bloke.

--- chooser.py
+++ chooser.py
@@ -86,41 +86,45 @@ names = [
 ]
 
 ##### Frontend using React
-##### ##### ##### #### ^^ What Windows Vista does a tremendously piss-poor job of doing.
+##### ##### ##### #### ^^ What Windows Vista does a tremendously piss-poor job of doing in any scenario.
+
+##### Боже мой, that's it! GitHub pages! It can be hosted there!
+##### https://gwenmurphy.github.io/{this thing}
+
+
 
 countrylist = range(len(countries))
 namelist = range(len(names))
 
 def assignTeamToPerson():
-    ##### Shuffles both the lists so it's harder to guess which person gets which team.
-    # random.shuffle(countries)
-    # random.shuffle(names)
-
     for i in range(0, len(countries)):
         ##### Shuffles them every time so nobody knows who gets what team.
-
+        ##### Yes, it shuffles every time. Odds of getting what you want
+        ##### are 1,023 to 1.
         random.shuffle(countries)
         random.shuffle(names) 
         
         ##### Sets a timer so the output is not zerg-rushed.
-        time.sleep(1)
+        ##### Yes, I know you understand that reference.
+        time.sleep(2)
         
         ##### Randomly assigns a country to a person. Doesn't look random, but it's shuffled
-        ##### every time it loops.
+        ##### every time it loops. Any more shuffling and the Goonies will be summoned.
         randomPerson = names[0]
         randomTeam = countries[0]
         print(f'May the odds be ever in your favour. Here\'s number {i+1}! {randomPerson} gets {randomTeam}!')
+        ##### Yes, that's a Hunger Games reference.
 
         ##### Accounting for specific circumstances.
         if randomPerson == "Will" and randomTeam == "Saudi Arabia":
             print("F in the chat for Will.")
+            ##### If this if statement is triggered, I implore someone to take a picture of the
+            ##### astounded look on his face. Bet it'll be like the Shocked Pikachu meme.
 
         ##### Removes what was just printed from the list.
         countries.pop(0)
         names.pop(0)
         print(f'{len(countries)} left to choose...')        
-
-
 
 
 assignTeamToPerson()
